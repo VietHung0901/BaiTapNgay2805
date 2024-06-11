eeab45fd03f14971689c56719d70b3e33152efb9
Tran Viet Hung <hungtgdd68@gmail.com>
2024-06-11 23:15:50 +0700

    Bai1106

--- bai2805/src/main/java/com/example/bai2805/Repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bai2805.Repositories;
-
-import com.example.bai2805.Entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,String> {
-}
--- bai2805/src/main/java/com/example/bai2805/Repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bai2805.Repositories;
-
-import com.example.bai2805.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,String> {
-}
